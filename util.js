c2e36c1700b1ad968e0aedff81edacdb0045fb7d
Benjamin Bloomfield <benjamin.bloomfield@travelgpa.com>
2017-01-19 14:41:26 -0500

    Removed things not necessary for gh-pages

--- util.js
+++
@@ -1,5 +0,0 @@
-var findThirdDeclensionRoot = function(nom,gen) {
-  var nomEnd = nom.match(/^(.*?)(\^?[aeiouy][^aeiouy,;.]*)[,;.\s]*$/)[1];
-  var genEnd = gen.match(/(\^?[aeiouy][^,;.]*)[,;.\s]*$/)[1];
-  return (nomEnd + genEnd).slice(0,-2);
-}
