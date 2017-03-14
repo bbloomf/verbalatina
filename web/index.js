6a2fcf402e637a96d3e8b5efc8e7e5ba1968143a
Benjamin Bloomfield <benjamin.bloomfield@travelgpa.com>
2017-03-14 17:02:05 -0400

    replace dibreve with a connecting double macron This really ought to be a double breve sometimes, or an inverted double breve, but I don’t know how to tell from the context.

--- web/index.js
+++ web/index.js
@@ -95,7 +95,7 @@ $(function() {
         return vowel + '\u0306'; // combining breve (˘)
       }).replace(/([ăĕĭŏŭ])_/gi, function(match, vowel) {
         return vowel + '\u0304'; // combining macron (¯)
-      }));
+      }).replace(/\[dibreve\]/g, '\u035e'));
       $('#dictionary .content foreign[lang=greek]').each(function() {
         var $this = $(this);
         $this.text(betaCodeToGreek($this.text()));
